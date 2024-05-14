31a49a0b0a3f90522d90d5ab27e893667182923c
Marcio Cruz de Almeida <marciocadev@gmail.com>
2024-05-14 08:25:47 -0300

    Initial commit

---
+++ test/basic-auth-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BasicAuthAws from '../lib/basic-auth-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/basic-auth-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BasicAuthAws.BasicAuthAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
